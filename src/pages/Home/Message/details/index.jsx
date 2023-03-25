dd6aab4700cbc395adc89598a8a1657e1b29fa6f
小熊饼干 <1125354737@qq.com>
2023-03-25 15:26:00 +0800

    feat：路由传参4种方式

--- src/pages/Home/Message/details/index.jsx
+++ src/pages/Home/Message/details/index.jsx
@@ -1,5 +1,5 @@
 import React,  {Component} from "react";
-
+// import qs from 'qs'; 
 const data =[
     { id: '1', content:'努力'},
     { id: '2', content:'上进'},
@@ -12,9 +12,25 @@ export default class Details extends Component {
 
 
     render() {
+        // 1、params传惨组件接收参数
         // 在this.props.match.params中可以拿到路由传递过来的参数
-        console.log('Details', this.props);
-        const {id, title} = this.props.match.params
+        // const {id, title} = this.props.match.params
+
+        // 2、search传惨组件接收参数
+        // search 传参在 在this.props.location.search中可以拿到，拿到的是字符串urlencode类型，需要用qs转换成对象类型
+        // const str = this.props.location.search // str拿到的字符串是?id=xx&title=xx str.slice(1)去除?
+        // const res = qs.parse(str.slice(1))
+        // const {id, title} = res
+
+        // 3、query传参，组件内接收
+        // 组件接收在this.props.location.query中拿到传递的参数
+        // const {id = 1 ,title = 2} = this.props.location.query
+
+
+        // 4、state传参，组件内接收
+        // 组件接收在this.props.location.state中拿到传递的参数
+        console.log(this.props);
+        const {id = 1 ,title = 2} = this.props.location.state
         const content = data.find(value => value.id === id)
         return (
             <div>
--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -25,6 +25,16 @@ export default class Message extends Component {
         ]
     }
 
+    handleClick = () =>{
+        // params 编程式导航
+        // this.props.history.push('/home/message/details/1/xxx')
+        // search 编程式导航
+        // this.props.history.push({pathname:'/home/message/details',search:'?id=1&title=2'})
+        // query 和 state 编程式导航
+        // this.props.history.push({pathname:'/home/message/details',query:{id:1, title:2}})
+        this.props.history.push({pathname:'/home/message/details',state:{id:'1', title:2}})
+    }
+
     render() {
         const {list} = this.state
         return(
@@ -33,17 +43,29 @@ export default class Message extends Component {
                {
                 list.map(item =>{
                     return (
-                        <li key={item.id}>
-                            {/* react路由传惨，params传惨。 声明路由并且传递数据*/}
-                            <Link to={`/home/message/details/${item.id}/${item.label}`}>{item.label}</Link>
+                        <li key={item.id} onClick={this.handleClick}>
+                            {/* 声明式导航 */}
+                            {/* 1、react路由传参，params传参。 声明路由并且传递数据*/}
+                            {/* <Link to={`/home/message/details/${item.id}/${item.label}`}>{item.label}</Link> */}
+                            {/* 2、react路由传参，search传参 */}
+                            {/* <Link to={`/home/message/details?id=${item.id}&title=${item.label}`}>{item.label}</Link> */}
+                            {/* 3、react路由传参，query传参 */}
+                            {/* <Link to={{pathname:'/home/message/details',query:{id:item.id, title:item.label}}}>{item.label}</Link> */}
+                            {/* 4、react路由传参，state传参  state和query传参其实是一样的，query明文，state加密 */} 
+                            {/* <Link to={{pathname:'/home/message/details',state:{id:item.id, title:item.label}}}>{item.label}</Link> */}
+                            <div >{item.label}</div>
                         </li>
                     )
                 })
                }
                </ul>
                <div>
-                {/* 注册路由组件，路径上接收声明的参数 */}
+                {/* 1、react路由传参，params传参。注册路由组件，路径上接收声明的参数 */}
                 <Route path='/home/message/details/:id/:title' component={Details}></Route>
+                {/* 2、serarch传参在组册路由组件时无需做任何事情 */}
+                {/* <Route path='/home/message/details' component={Details}></Route> */}
+                {/* 3、query， state传参在组册路由组件时无需做任何事情 */}
+                <Route path='/home/message/details' component={Details}></Route>
                </div>
             </div>
         )
